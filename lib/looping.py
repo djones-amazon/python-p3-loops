9c2433377e65c7b1d849156fa090e5b6b93ee2b9
Devin Jones <devin.jones21083@gmail.com>
2023-07-27 00:44:31 -0400

    Passing all tests

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count_num = 10
+    while count_num > 0:
+        print(count_num)
+        count_num -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_list = [num * num for num in int_list]
+    return square_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    
+    for num in range(1, 101):
+        if (int(num) % 15) == 0:
+            print("FizzBuzz")
+        elif (int(num) % 3) == 0:
+            print("Fizz")
+        elif (int(num) % 5) == 0:
+            print("Buzz")
+        else:
+            print(num)
